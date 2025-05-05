f5c84908d5ca886ddad1f7176edc25c95ba6a82f
Антон <vasin.anton.work@gmail.com>
2025-05-05 14:39:15 +0300

    Удаление старой базы данных из-за изменений столбцов числа таблицы.

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./minimal_db.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Используй относительный путь, если база лежит в корне проекта
+SQLALCHEMY_DATABASE_URL = "sqlite:///./new_minimal_db.db"
+
+# Для SQLite обязательно добавлять connect_args={"check_same_thread": False}
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
