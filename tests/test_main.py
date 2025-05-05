e881bf37390779b9fdbad6cf07014ea007a4c19a
Антон <vasin.anton.work@gmail.com>
2025-05-05 21:42:23 +0300

    Добавление тестов часть 1 (проверка подключения абилок и снаряжения)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi.testclient import TestClient
+from main import app
+
+# некоторые изменения в сявзяи с проблемами с путями
 import pytest
 from fastapi.testclient import TestClient
 from main import app
